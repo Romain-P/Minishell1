76663d4ebfa410989e566b55d07c7897abdc2c06
Romain <romain.pillot@epitech.eu>
2017-03-10 19:17:35 +0000

    Checking if parent has child proccesses to display prompt on ^C

--- src/shell.c
+++ src/shell.c
@@ -5,7 +5,7 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Fri Mar  3 02:18:12 2017 romain pillot
-** Last update Thu Mar  9 16:29:46 2017 romain pillot
+** Last update Fri Mar 10 19:16:05 2017 romain pillot
 */
 
 #include "minishell.h"
@@ -15,6 +15,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+
+static bool	has_child;
 
 static void	display_prompt()
 {
@@ -37,7 +39,8 @@ static void	display_prompt()
 static void     exit_handle()
 {
   display_char('\n');
-  display_prompt();
+  if (!has_child)
+    display_prompt();
 }
 
 void	launch(t_shell *shell, int file)
@@ -48,12 +51,14 @@ void	launch(t_shell *shell, int file)
   signal(SIGINT, &exit_handle);
   while (shell->status == -1)
     {
+      has_child = false;
       if (shell->isatty)
 	display_prompt();
       if (!(cmd_line = scan_line(file)))
 	shell->exit(shell, EXIT_SUCCESS, shell->isatty ? "exit\n" : NULL);
       else
 	{
+	  has_child = true;
 	  args = splitstr(cmd_line, ' ');
 	  if (args && *args)
 	    shell->cmds[get_cmd_index(*args)](shell, args);
